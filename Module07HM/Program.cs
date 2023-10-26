f689ea95cef4b01029e77722f1bc8f3d497825f8
Timerlan01 <alimbekovtimerlan@gmail.com>
2023-10-26 09:15:33 +0600

    Добавьте файлы проекта.

---
+++ Module07HM/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordOfTanks;
+
+namespace Module07HM
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Tank[] tanks = new Tank[10];
+
+            for (int i = 0; i < 5; i++)
+            {
+                tanks[i] = new Tank("Т-34");
+                tanks[i + 5] = new Tank("Pantera");
+            }
+
+            // Проведение битв
+            for (int i = 0; i < 5; i++)
+            {
+                if (tanks[i] ^ tanks[i + 5])
+                    Console.WriteLine($"{tanks[i].GetTankParameters()} победил в сражении с {tanks[i + 5].GetTankParameters()}");
+                else
+                    Console.WriteLine($"{tanks[i + 5].GetTankParameters()} победил в сражении с {tanks[i].GetTankParameters()}");
+            }
+        }
+    }
+}
---
+++ WordOfTanks/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordOfTanks
+{
+    public class Tank
+    {
+        public string tankName;
+        public int ammunitionLevel;
+        public int armorLevel;
+        public int agilityLevel;
+        public Random random;
+
+        public Tank(string name)
+        {
+            tankName = name;
+            random = new Random();
+
+            // Инициализация случайными значениями от 0 до 100 для параметров танка
+            ammunitionLevel = random.Next(101);
+            armorLevel = random.Next(101);
+            agilityLevel = random.Next(101);
+        }
+        public string GetTankParameters()
+        {
+            return $"{tankName}: Боекомплект - {ammunitionLevel}%, Броня - {armorLevel}%, Маневренность - {agilityLevel}%";
+        }
+        public static bool operator ^(Tank tank1, Tank tank2)
+        {
+            // Проверка победы одного танка над другим
+            int winCriteria = 0;
+            if (tank1.ammunitionLevel > tank2.ammunitionLevel)
+                winCriteria++;
+            if (tank1.armorLevel > tank2.armorLevel)
+                winCriteria++;
+            if (tank1.agilityLevel > tank2.agilityLevel)
+                winCriteria++;
+            return winCriteria >= 2; // Победа, если выполнено не менее двух критериев
+        }
+    }
+}
